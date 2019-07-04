074a98ae6257e4546e9674b420e4525eb5f7bea6
Donald Labaj <donald.labaj@gmail.com>
2019-07-04 10:14:36 -0400

    Updates to app and appSidebar

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,43 +1,18 @@
 import React, { Component, ReactNode } from "react";
 import {
-  BackgroundImage,
-  BackgroundImageSrc,
   Page,
   PageSectionVariants,
-  NavSelectClickHandler,
   PageSection,
   TextContent,
   Text
 } from "@patternfly/react-core";
 import AppHeader from "./appHeader";
 import AppSidebar from "./appSidebar";
-import xs from "@pf4-assets/images/pfbg_576.jpg";
-import xs2x from "@pf4-assets/images/pfbg_576@2x.jpg";
-import sm from "@pf4-assets/images/pfbg_768.jpg";
-import sm2x from "@pf4-assets/images/pfbg_768@2x.jpg";
-import lg from "@pf4-assets/images/pfbg_1200.jpg";
-import filter from "@pf4-assets/images/background-filter.svg";
-
-const images = {
-  [BackgroundImageSrc.xs]: xs,
-  [BackgroundImageSrc.xs2x]: xs2x,
-  [BackgroundImageSrc.sm]: sm,
-  [BackgroundImageSrc.sm2x]: sm2x,
-  [BackgroundImageSrc.lg]: lg,
-  [BackgroundImageSrc.filter]: `${filter}#image_overlay`
-};
 
 export default class App extends Component {
   public state = {
     activeMenuGroup: "",
     activeMenuGroupItem: ""
-  };
-
-  onNavSelect = ({ groupId, itemId }: any) => {
-    this.setState({
-      activeMenuGroup: groupId,
-      activeMenuGroupItem: itemId
-    });
   };
 
   public render() {
@@ -69,4 +44,11 @@ export default class App extends Component {
       </React.Fragment>
     );
   }
+
+  private onNavSelect = ({ groupId, itemId }: any) => {
+    this.setState({
+      activeMenuGroup: groupId,
+      activeMenuGroupItem: itemId
+    });
+  };
 }
--- src/app/appSidebar.tsx
+++ src/app/appSidebar.tsx
@@ -19,74 +19,74 @@ export const AppSidebar: React.FunctionComponent<AppSidebarProps> = ({
   activeMenuGroupItem,
   ...props
 }: AppSidebarProps) => {
-  const sideBarNavigation = (<Nav onSelect={onSelect} aria-label="Nav">
-  <NavList>
-    <NavItem
-    groupId="grp-1"
-    itemId="grp-1_itm-1"
-    isActive={activeMenuGroupItem === "grp-1_itm-1"}
-    >
-    Dashboard
-    </NavItem>
-    <NavExpandable
-      title="APIs"
-      groupId="grp-2"
-      isActive={activeMenuGroup === "grp-2"}
-    >
-      <NavItem
-        groupId="grp-2"
-        itemId="grp-2_itm-1"
-        isActive={activeMenuGroupItem === "grp-2_itm-1"}
-      >
-        View All APIs
-      </NavItem>
-      <NavItem
-        groupId="grp-2"
-        itemId="grp-2_itm-2"
-        isActive={activeMenuGroupItem === "grp-2_itm-2"}
-      >
-        Create New API
-      </NavItem>
-      <NavItem
-        groupId="grp-3"
-        itemId="grp-2_itm-3"
-        isActive={activeMenuGroupItem === "grp-2_itm-3"}
-      >
-        Import API
-      </NavItem>
-    </NavExpandable>
-    <NavExpandable
-      title="Settings"
-      groupId="grp-3"
-      isActive={activeMenuGroup === "grp-3"}
-    >
-      <NavItem
-        groupId="grp-3"
-        itemId="grp-3_itm-1"
-        isActive={activeMenuGroupItem === "grp-3_itm-1"}
-      >
-        User Profile
-      </NavItem>
-      <NavItem
-        groupId="grp-3"
-        itemId="grp-3_itm-2"
-        isActive={activeMenuGroupItem === "grp-3_itm-2"}
-      >
-        Linked Accounts
-      </NavItem>
-      <NavItem
-        groupId="grp-3"
-        itemId="grp-3_itm-3"
-        isActive={activeMenuGroupItem === "grp-3_itm-3"}
-      >
-        Validation
-      </NavItem>
-    </NavExpandable>
-  </NavList>
-</Nav>)
-  return (
-    <PageSidebar nav={sideBarNavigation} />
+  const sideBarNavigation = (
+    <Nav onSelect={onSelect} aria-label="Nav">
+      <NavList>
+        <NavItem
+          groupId="grp-1"
+          itemId="grp-1_itm-1"
+          isActive={activeMenuGroupItem === "grp-1_itm-1"}
+        >
+          Dashboard
+        </NavItem>
+        <NavExpandable
+          title="APIs"
+          groupId="grp-2"
+          isActive={activeMenuGroup === "grp-2"}
+        >
+          <NavItem
+            groupId="grp-2"
+            itemId="grp-2_itm-1"
+            isActive={activeMenuGroupItem === "grp-2_itm-1"}
+          >
+            View All APIs
+          </NavItem>
+          <NavItem
+            groupId="grp-2"
+            itemId="grp-2_itm-2"
+            isActive={activeMenuGroupItem === "grp-2_itm-2"}
+          >
+            Create New API
+          </NavItem>
+          <NavItem
+            groupId="grp-3"
+            itemId="grp-2_itm-3"
+            isActive={activeMenuGroupItem === "grp-2_itm-3"}
+          >
+            Import API
+          </NavItem>
+        </NavExpandable>
+        <NavExpandable
+          title="Settings"
+          groupId="grp-3"
+          isActive={activeMenuGroup === "grp-3"}
+        >
+          <NavItem
+            groupId="grp-3"
+            itemId="grp-3_itm-1"
+            isActive={activeMenuGroupItem === "grp-3_itm-1"}
+          >
+            User Profile
+          </NavItem>
+          <NavItem
+            groupId="grp-3"
+            itemId="grp-3_itm-2"
+            isActive={activeMenuGroupItem === "grp-3_itm-2"}
+          >
+            Linked Accounts
+          </NavItem>
+          <NavItem
+            groupId="grp-3"
+            itemId="grp-3_itm-3"
+            isActive={activeMenuGroupItem === "grp-3_itm-3"}
+          >
+            Validation
+          </NavItem>
+        </NavExpandable>
+      </NavList>
+    </Nav>
   );
+  return <PageSidebar nav={sideBarNavigation} />;
 };
 
 export default AppSidebar;
