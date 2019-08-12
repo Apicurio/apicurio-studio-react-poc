6f553f4be1c83227a5db29e3bc32383fd81e169c
Donald Labaj <donald.labaj@gmail.com>
2019-08-12 14:25:42 -0400

    Added model packages for apicurio.

---
+++ packages/models/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'apicurio-models',
+    umdNamedDefine: true
+  }
+};
