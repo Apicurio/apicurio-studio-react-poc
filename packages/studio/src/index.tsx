da80101537e885dd9ffe49edddc0106286d770cf
Donald Labaj <donald.labaj@gmail.com>
2019-08-27 16:02:24 -0400

    Updates for keycloak

--- packages/studio/src/index.tsx
+++ packages/studio/src/index.tsx
@@ -3,25 +3,27 @@ import ReactDOM from "react-dom";
 import "@patternfly/react-core/dist/styles/base.css";
 import App from './app/app';
 import './app/app.css';
+import './config.js';
+import Keycloak from './keycloak';
+import './version.js';
 
 // Handle login via keycloak
 
-// let keycloak = window["Keycloak"]();
-// keycloak.init({onLoad: 'login-required'}).success(function (authenticated) {
-//     if (authenticated) {
-//         window['keycloak'] = keycloak;
-//         platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
-//             printBanner("Development");
-//         }).catch(err => console.log(err));
-//     }
-// }).error(function () {
-//     alert('Failed to initialize authentication subsystem.');
-// });
+debugger;
+const keycloak = Keycloak();
+keycloak.init({onLoad: 'login-required'}).success((authenticated: any) => {
+    if (authenticated) {
+        (window as any).keycloak = keycloak;
+        if (process.env.NODE_ENV !== "production") {
+          // tslint:disable-next-line
+          const axe = require("react-axe");
+          axe(React, ReactDOM, 1000);
+        }
+        
+        ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+    }
+}).error(() => {
+    alert('Failed to initialize authentication subsystem.');
+});
 
-if (process.env.NODE_ENV !== "production") {
-  // tslint:disable-next-line
-  const axe = require("react-axe");
-  axe(React, ReactDOM, 1000);
-}
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
