77f4184e2bb565b2bd09e7351b76cd31cd94b151
Donald Labaj <donald.labaj@gmail.com>
2019-09-25 14:16:10 -0400

    Fixed issue with keycloak

--- packages/studio/webpack.dev.js
+++ packages/studio/webpack.dev.js
@@ -3,7 +3,7 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 const HOST = process.env.HOST || "localhost";
-const PORT = process.env.PORT || "9000";
+const PORT = process.env.PORT || "8888";
 
 module.exports = merge(common, {
   mode: "development",
