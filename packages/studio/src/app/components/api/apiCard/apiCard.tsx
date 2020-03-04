ba1d9bb87dcb5ce5fdffc121383539e44a2f5726
jenny-s51 <jshandel12@gmail.com>
2020-03-04 16:30:10 -0500

    rebasing mark's changes
    working card view
    
    working card view
    
    card view, appToolbar WIP
    
    adding UI components
    
    updates to appToolbar, appCardview. moving state functions to toolbar
    
    adding context for card view
    
    adding props to be passed to context from state
    
    removing state functions from context
    
    addressing some PR feedback, formatting, WIP
    
    renaming attributes
    
    adds selected items to array
    
    rebasing with master
    
    deleting some files
    
    adding back api-data.json
    
    commenting out data list for now
    
    commenting out context-related state mgmt. -- refactor to hooks in progress
    
    refactor to function component with hooks, WIP
    
    change to DataToolbarGroup for filter/search
    
    rebased with master, checkboxes are working with bulk-select now, WIP
    
    splitCheckboxSelectAll is working in bulk-select, working on other select functions
    
    working on bulk-select functions
    
    bulk-select and alphabetic sorting is done, ready for review
    
    rebased with context changes

--- packages/studio/src/app/components/api/apiCard/apiCard.tsx
+++ packages/studio/src/app/components/api/apiCard/apiCard.tsx
@@ -1,37 +1,63 @@
+
 import React from 'react';
-import { Card, CardHead, CardHeader, CardBody, CardFooter, CardActions } from '@patternfly/react-core';
-import {ApiTag} from '../apiTag';
-import ApicurioIcon from '../../../assets/apicurio-icon.png';
-import './apiCard.css'
+// import { Card, CardHead, CardHeader, CardBody, CardFooter, CardActions, Gallery, Dropdown, KebabToggle, DropdownItem, DropdownSeparator, Checkbox } from '@patternfly/react-core';
+// import {ApiTag} from '../apiTag';
+// import ApicurioIcon from './assets/apicurio-icon.png';
+// import './apiCard.css'
+// import { useStoreContext } from '../../../../context/reducers';
+// import { ApiDropdownKebab } from './../apiDropDownKebab';
 
-interface ApiCardProps {
-  id: string,
-  name: string,
-  description: string,
-  tags: string[],
-  type?: string
-}
+// interface AppCardProps {
+//   id?: string,
+//   name?: string,
+//   description?: string,
+//   tags?: string[],
+//   type?: string
+// }
 
-export const ApiCard: React.FunctionComponent<ApiCardProps> = ({ name, description, tags = []}: ApiCardProps) => {
-  return (
-    <Card>
-      <CardHead>
-        <img src={ApicurioIcon}/>
-        <CardActions/>
-      </CardHead>
-      <CardHeader className="api-card-view-card-header">
-        {name}
-      </CardHeader>
-      <CardBody className="api-card-view-card-body">
-        {description}
-      </CardBody>
-      <CardFooter>
-        <div className="app-api-tag-group">
-          {tags.map((tag, index) => 
-            <ApiTag key={index} text={tag}/>
-          )}
-        </div>
-      </CardFooter>
-    </Card>
-  );
-}
+// export const AppCard: React.FunctionComponent<AppCardProps> = ({ name, description, tags = []}: AppCardProps) => {
+//     const { apiData } = useStoreContext();
+
+//     return (
+
+//         <Gallery gutter="md">
+ 
+//          {apiData.map((api, key) => (
+//            <React.Fragment>
+//              <Card key={key}>
+//                <CardHead>
+//                  <img src={ApicurioIcon} />
+//                  <CardActions>
+//                    <ApiDropdownKebab/>
+//                    <Checkbox
+//                      value={api.id}
+//                     //  isChecked={selectedItems.includes(parseInt(api.id))}
+//                     //  checked={isChecked}
+//                     //  onChange={this.handleCheckboxClick}
+//                     //  aria-label="card checkbox example"
+//                      id="check-1"
+//                     //  name="check1"
+//                    />
+//                  </CardActions>
+//                </CardHead>
+//                <CardHeader className="app-card-view-card-header">
+//                  {api.name}
+//                </CardHeader>
+//                <CardBody className="app-card-view-card-body">
+//                  {api.description}
+//                </CardBody>
+//                <CardFooter>
+//                  <div className="app-api-tag-group">
+//                    {api.tags.map(
+//                      (tag: string, index: string | number | undefined) => (
+//                        <ApiTag text={tag} />
+//                      )
+//                    )}
+//                  </div>
+//                </CardFooter>
+//              </Card>
+//            </React.Fragment>
+//          ))}
+//        </Gallery>
+//      );
+// }
--- packages/studio/src/app/components/api/apiToolbar/apiToolbar.tsx
+++ packages/studio/src/app/components/api/apiToolbar/apiToolbar.tsx
@@ -1,27 +1,246 @@
-import React, { useContext } from 'react';
-import { DataToolbar , DataToolbarItem, DataToolbarContent } from '@patternfly/react-core/dist/esm/experimental';
-import { Button } from '@patternfly/react-core';
-import {ThIcon, ListIcon} from '@patternfly/react-icons';
-import {GlobalContext, GlobalContextObj, DashboardViews} from '../../../../context';
+import React, { useContext, useState } from "react";
+import {
+  DataToolbar,
+  DataToolbarItem,
+  DataToolbarContent
+} from "@patternfly/react-core/dist/esm/experimental";
+import {
+  Button,
+  Dropdown,
+  DropdownPosition,
+  DropdownToggle,
+  DropdownItem,
+  DropdownToggleCheckbox,
+  InputGroup,
+  TextInput,
+  ButtonVariant,
+  DataToolbarGroup
+} from "@patternfly/react-core";
+import {
+  ThIcon,
+  ListIcon,
+  FilterIcon,
+  SearchIcon,
+  SortAlphaDownIcon,
+  SortAlphaUpIcon
+} from "@patternfly/react-icons";
+import { Api } from "@apicurio/models";
+import {
+  GlobalContext,
+  GlobalContextObj,
+  DashboardViews,
+  ToolbarStatus
+} from "../../../../context";
 
-export const ApiToolbar: React.FunctionComponent = () => {
-
+export const ApiToolbar = () => {
   const globalContext: GlobalContextObj = useContext(GlobalContext);
 
   const apiCount = globalContext.store.apis.length;
+  const [splitButtonDropdownIsOpen, setSplitButtonDropdownIsOpen] = useState(
+    false
+  );
+  const [isLowerToolbarDropdownOpen, setIsLowerToolbarDropdownOpen] = useState(
+    false
+  );
+  const [sortIconChanged, setSortIconChanged] = useState(false);
+  const [currentCategory, setCurrentCategory] = useState("Name");
+
+  const compare = (direction: string) => {
+    if (direction === "asc") {
+      return (a: Api, b: Api) => (a.name > b.name ? 1 : -1);
+    } else if (direction === "desc") {
+      return (a: Api, b: Api) => (b.name > a.name ? 1 : -1);
+    }
+    return (a: Api, b: Api) => 0;
+  };
+
+  const sortAlphaDown = () => {
+    globalContext.updateApis(globalContext.store.apis.sort(compare("desc")));
+    setSortIconChanged(true);
+  };
+
+  const sortAlphaUp = () => {
+    globalContext.updateApis(globalContext.store.apis.sort(compare("asc")));
+    setSortIconChanged(false);
+  };
+
+  const onNameSelect = (event: any) => {
+    setCurrentCategory(event.target.innerText);
+    setIsLowerToolbarDropdownOpen(!isLowerToolbarDropdownOpen);
+  };
+
+  const onToolbarDropdownToggle = (event: any) => {
+    setIsLowerToolbarDropdownOpen(!isLowerToolbarDropdownOpen);
+  };
+
+  const selectAll = () => {
+    let collection: number[] = [];
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < globalContext.store.apis.length; i++) {
+      // tslint:disable-next-line: radix
+      collection = [...collection, parseInt(globalContext.store.apis[i].id)];
+    }
+
+    globalContext.updateToolbarStatus({
+      areAllSelected: true,
+      isChecked: true,
+      selectedItems: collection
+    });
+  };
+
+  const splitCheckboxSelectAll = (e: any) => {
+    const { checked } = e.target;
+    let collection: number[] = [];
+
+    if (checked) {
+      // tslint:disable-next-line: prefer-for-of
+      for (let i = 0; i < globalContext.store.apis.length; i++) {
+        // tslint:disable-next-line: radix
+        collection = [...collection, parseInt(globalContext.store.apis[i].id)];
+      }
+    }
+
+    globalContext.updateToolbarStatus({
+      areAllSelected: checked,
+      isChecked: checked,
+      selectedItems: collection
+    });
+  };
+
+  const selectNone = (e: any) => {
+    globalContext.updateToolbarStatus({
+      areAllSelected: false,
+      isChecked: false,
+      selectedItems: []
+    });
+  };
+
+  const onSplitButtonToggle = (isOpen: boolean) => {
+    setSplitButtonDropdownIsOpen(isOpen);
+  };
+
+  const onSplitButtonSelect = (event: any) => {
+    setSplitButtonDropdownIsOpen(!splitButtonDropdownIsOpen);
+  };
+
+  const buildFilterDropdown = () => (
+    <DataToolbarItem>
+      <Dropdown
+        onSelect={onNameSelect}
+        position={DropdownPosition.left}
+        toggle={
+          <DropdownToggle
+            onToggle={onToolbarDropdownToggle}
+            style={{ width: "100%" }}
+          >
+            <FilterIcon /> {currentCategory}
+          </DropdownToggle>
+        }
+        isOpen={isLowerToolbarDropdownOpen}
+        dropdownItems={[
+          <DropdownItem key="cat1">Name</DropdownItem>,
+          <DropdownItem key="cat2">Tag</DropdownItem>
+        ]}
+        style={{ width: "100%" }}
+      />
+    </DataToolbarItem>
+  );
+
+  const buildSelectDropdown = () => {
+    const numSelected = globalContext.store.toolbarStatus.selectedItems.length;
+    const allSelected = globalContext.store.toolbarStatus.areAllSelected;
+    const anySelected = numSelected > 0;
+
+    return (
+      <Dropdown
+        position={DropdownPosition.left}
+        onSelect={onSplitButtonSelect}
+        toggle={
+          <DropdownToggle
+            splitButtonItems={[
+              <DropdownToggleCheckbox
+                id="dropdown-checkbox"
+                key="split-checkbox"
+                aria-label={anySelected ? "Deselect all" : "Select all"}
+                isChecked={allSelected}
+                onClick={splitCheckboxSelectAll}
+              />
+            ]}
+            onToggle={onSplitButtonToggle}
+          >
+            {numSelected !== 0 && (
+              <React.Fragment>{numSelected} selected</React.Fragment>
+            )}
+          </DropdownToggle>
+        }
+        isOpen={splitButtonDropdownIsOpen}
+        dropdownItems={[
+          <DropdownItem key="item-1" onClick={selectNone}>
+            Select none (0 items)
+          </DropdownItem>,
+          <DropdownItem key="item-3" onClick={selectAll}>
+            Select all ({globalContext.store.apis.length} items)
+          </DropdownItem>
+        ]}
+      />
+    );
+  };
 
   return (
     <DataToolbar id="apiToolbar">
       <DataToolbarContent>
+        <DataToolbarItem>{buildSelectDropdown()}</DataToolbarItem>
+        <DataToolbarGroup variant="filter-group">
+          {buildFilterDropdown()}
+          <InputGroup>
+            <TextInput
+              name="textInput1"
+              id="textInput1"
+              type="search"
+              aria-label="search input"
+              css=""
+            />
+            <Button
+              variant={ButtonVariant.control}
+              aria-label="search button for search input"
+            >
+              <SearchIcon />
+            </Button>
+          </InputGroup>
+        </DataToolbarGroup>
         <DataToolbarItem>
-          This is where the Data Toolbar should go
+          <Button
+            variant="plain"
+            onClick={sortIconChanged ? sortAlphaUp : sortAlphaDown}
+          >
+            {sortIconChanged ? <SortAlphaDownIcon /> : <SortAlphaUpIcon />}
+          </Button>
         </DataToolbarItem>
         <DataToolbarItem variant="pagination">
-          <Button onClick={() => globalContext.setDashboardView(DashboardViews.card)} className={'app-data-toolbar-button-control ' + (globalContext.store.dashboardView === DashboardViews.card ? "pf-m-selected" : "")} variant="plain">
-            <ThIcon/>
+          <Button
+            onClick={() => globalContext.setDashboardView(DashboardViews.card)}
+            className={
+              "app-data-toolbar-button-control " +
+              (globalContext.store.dashboardView === DashboardViews.card
+                ? "pf-m-selected"
+                : "")
+            }
+            variant="plain"
+          >
+            <ThIcon />
           </Button>
-          <Button onClick={() => globalContext.setDashboardView(DashboardViews.list)} className={'app-data-toolbar-button-control ' + (globalContext.store.dashboardView === DashboardViews.list ? "pf-m-selected" : "")} variant="plain">
-            <ListIcon/>
+          <Button
+            onClick={() => globalContext.setDashboardView(DashboardViews.list)}
+            className={
+              "app-data-toolbar-button-control " +
+              (globalContext.store.dashboardView === DashboardViews.list
+                ? "pf-m-selected"
+                : "")
+            }
+            variant="plain"
+          >
+            <ListIcon />
           </Button>
           <span className="app-toolbar-api-total">
             {apiCount} APIs found
@@ -29,7 +248,7 @@ export const ApiToolbar: React.FunctionComponent = () => {
         </DataToolbarItem>
       </DataToolbarContent>
     </DataToolbar>
-  )
-}
+  );
+};
 
 export default ApiToolbar;
