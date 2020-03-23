7c6a020b8e6591c9130ea1d42ecc4a4e67f22f51
Christie Molloy <cmolloy@redhat.com>
2020-03-23 17:10:05 -0400

    start masthead

--- packages/studio/src/app/appHeader.tsx
+++ packages/studio/src/app/appHeader.tsx
@@ -1,13 +1,73 @@
-import React from 'react';
-import {Avatar, Brand, PageHeader } from '@patternfly/react-core';
+import React, {useState} from 'react';
+import {
+  Avatar,
+  Brand,
+  Button,
+  ButtonVariant,
+  Dropdown,
+  Toolbar,
+  KebabToggle,
+  ToolbarItem,
+  ToolbarGroup,
+  PageHeader
+} from '@patternfly/react-core';
+import { BellIcon, CogIcon } from '@patternfly/react-icons';
 import brandImg from '../../assets/images/apicurio_logo_darkbkg_200px.png';
+import imgAvatar from '../../assets/images/avatar_image.svg';
+import accessibleStyles from '@patternfly/react-styles/css/utilities/Accessibility/accessibility';
+import spacingStyles from '@patternfly/react-styles/css/utilities/Spacing/spacing';
+import { css } from '@patternfly/react-styles';
 
-const showNavToogle: boolean = true;
+export const AppHeader = () => {
+    const [isKebabDropdownOpen, setKebabDropdown] = useState(false);
 
-export const AppHeader: React.FunctionComponent<any> = (props) => {
+    const onKebabDropdownToggle = () => {
+      setKebabDropdown(true);
+    }
+
+    const PageToolbar = (
+      <Toolbar>
+        <ToolbarGroup className={css(accessibleStyles.screenReader, accessibleStyles.visibleOnLg)}>
+          <ToolbarItem>
+            <Button id="" aria-label="" variant={ButtonVariant.plain}>
+              <BellIcon />
+            </Button>
+          </ToolbarItem>
+          <ToolbarItem>
+            <Button id="" aria-label="" variant={ButtonVariant.plain}>
+              <CogIcon/>
+            </Button>
+          </ToolbarItem>
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <ToolbarItem className={css(accessibleStyles.hiddenOnLg, spacingStyles.mr_0)}>
+            <Dropdown
+              isPlain
+              position="right"
+              onSelect={this.onKebabDropdownSelect}
+              toggle={<KebabToggle onToggle={onKebabDropdownToggle} />}
+              isOpen={isKebabDropdownOpen}
+              dropdownItems={kebabDropdownItems}
+            />
+          </ToolbarItem>
+          <ToolbarItem className={css(accessibleStyles.screenReader, accessibleStyles.visibleOnMd)}>
+            <Dropdown
+              isPlain
+              position="right"
+              onSelect={this.onDropdownSelect}
+              isOpen={isDropdownOpen}
+              toggle={<DropdownToggle onToggle={this.onDropdownToggle}>Username</DropdownToggle>}
+              dropdownItems={userDropdownItems}
+            />
+          </ToolbarItem>
+        </ToolbarGroup>
+      </Toolbar>
+    );
+
     return (<PageHeader
         logo={<Brand src={ brandImg } alt="Apicurio" />}
-        showNavToggle = {showNavToogle}
+        avatar={ <Avatar src={imgAvatar} alt="user image"/>}
+        toolbar={PageToolbar}
       />);
 }
 
