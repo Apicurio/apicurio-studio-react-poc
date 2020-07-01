4385b95b8610c675d51f713caf77c20224db469b
jenny-s51 <jshandel12@gmail.com>
2020-07-01 15:45:40 -0400

    fix loader errors for rh-uxd package

--- packages/studio/src/app/app.tsx
+++ packages/studio/src/app/app.tsx
@@ -4,35 +4,21 @@ import AppHeader from "./appHeader";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import * as Pages from './pages';
 import './app.css';
-import { LoadingPage } from '@rh-uxd/integration-react';
 
 export const App: React.FunctionComponent = () => {
 
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  const delayState = () => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  };
-
-  delayState();
-
   return (
       <Router>
-        <div style={{position: 'relative'}}>
-          { isLoading && <LoadingPage appName="Syndesis"/>}
-          <Page 
-            isManagedSidebar={true}
-            header={<AppHeader />}
-            className="app-page"
-          >
-            <Route path='/' exact={true} component={Pages.Dashboard}/>
-            <Route path='/dashboard' exact={true} component={Pages.Dashboard}/>
-            <Route path='/create-api' exact={true} component={Pages.CreateApi}/>
-            <Route path='/import-api' exact={true} component={Pages.ImportApi}/>
-          </Page>
-        </div>
+      <Page 
+        isManagedSidebar={true}
+        header={<AppHeader />}
+        className="app-page"
+      >
+        <Route path='/' exact={true} component={Pages.Dashboard}/>
+        <Route path='/dashboard' exact={true} component={Pages.Dashboard}/>
+        <Route path='/create-api' exact={true} component={Pages.CreateApi}/>
+        <Route path='/import-api' exact={true} component={Pages.ImportApi}/>
+      </Page>
       </Router>
   );
 }
